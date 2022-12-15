42b978d330a1b1ea654730f60240b10ccdc3f1e5
=Mack <=maaaaaack@outlook.com>
2022-12-15 13:52:52 -0300

    fix

---
+++ config.js
@@ -0,0 +1,96 @@
+const hostInfos = [
+  {
+    name: 'jailbreak',
+    port: '27170',
+    host: '131.196.196.197'
+  },
+  /* {
+    name: dr,
+    port: '27080'
+  }, */
+  {
+    name: 'awp',
+    port: '27090',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'retake',
+    port: '27130',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'retake',
+    port: '27150',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'mix',
+    port: '27190',
+    host: '131.196.196.197'
+  },
+
+  {
+    name: 'arena',
+    port: '27260',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'retake',
+    port: '27270',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'retake',
+    port: '27330',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'retake',
+    port: '27350',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'mix',
+    port: '27280',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'surf',
+    port: '27050',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'dm',
+    port: '27310',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'dm',
+    port: '27400',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'dm',
+    port: '27440',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'retake',
+    port: '27450',
+    host: '131.196.196.197'
+  },
+  {
+    name: 'mix',
+    port: '27110',
+    host: '131.196.196.196'
+  },
+  {
+    name: 'retake',
+    port: '27120',
+    host: '131.196.196.196'
+  },
+]
+
+module.exports = {
+  hostInfos
+}
--- index.js
+++ index.js
@@ -1,46 +1,90 @@
 const express = require('express')
-const config = require('./config.json')
+const serversJSON = require('./servers.json')
 const { query } = require('gamedig')
 const app = express()
 const { writeFileSync } = require('fs')
-
+const { hostInfos } = require('./config')
 
 app.get('/servers/:id', (request, response) => {
-  let server = config[request.params.id]
+  
+  let server = serversJSON.servers.find(sv => sv.name === request.params.id)
 
   if (!server) return response.status(404).json('Server not found')
 
-  if (new Date().getTime() - server.lastSync <= '60000') return response.json(server.servers)
+  //if (new Date().getTime() - serversJSON.lastSync <= '60000') return response.json(server)
+  return response.json(server.redirectTo) 
+})
+app.get('/servers', async (request, response) => {
 
-  async function GetServers() {
-    const servers = []
-    for (let serverPort of server.ports) {
+  //if (new Date().getTime() - serversJSON.lastSync <= '60000') return response.json(serversJSON.servers)
 
-      await query({
-        type: 'csgo',
-        host: '131.196.196.197',
-        port: serverPort,
-        maxAttempts: 7,
-      })
-        .then((state) => {
+  let servers = []
 
-          servers.push({
+  for (let serverPort of hostInfos) {
+
+    await query({
+      type: 'csgo',
+      host: serverPort.host,
+      port: serverPort.port,
+    })
+      .then((state) => {
+        let findSv = servers.find(sv => sv.name === serverPort.name)
+
+        findSv ?
+          findSv.serversInfos.push({
+            name: state.name,
+            map: state.map,
+            ip: state.connect,
             players: state.raw.numplayers,
-            ip: state.connect
+            playersTotal: state.maxplayers
           })
-        })
-        .catch(() => { })
+          :
+          servers.push({
+            name: serverPort.name,
+            redirectTo: '',
+            serversInfos: [
+              {
+                name: state.name,
+                map: state.map,
+                ip: state.connect,
+                players: state.raw.numplayers,
+                playersTotal: state.maxplayers
+              }
+            ],
+          })
+      })
+      .catch((err) => { console.log(err) })
+  }
+  //response.json(servers)
+  servers = servers.map(sv => {
+    if (sv.serversInfos.length <= 1) return sv;
+
+    let newSv = []
+
+    for (let i in sv.serversInfos) {
+      newSv.push((sv.serversInfos[i].players / sv.serversInfos[i].playersTotal).toFixed(2))
     }
 
-    return servers.reduce((min, cur) => min.players < cur.players ? min : cur)
-  }
-  GetServers().then((sv) => {
-    server.lastSync = new Date().getTime()
-    server.servers = sv
-    config[request.params.id] = server
-    writeFileSync('./config.json', JSON.stringify(config))
-    return response.json(sv)
+    let getServers = newSv.reduce(function (prev, curr) {
+      return (Math.abs(curr - 0.5) < Math.abs(prev - 0.5) ? curr : prev);
+    })
+    getServers = sv.serversInfos[newSv.findIndex(m => m === getServers)].ip
+
+    console.log(getServers)
+    /* let getServers = sv.serversInfos.reduce((acc, cur) => {
+      if (cur.players === cur.playersTotal || (cur.players === 0 && acc.players > 0) || (acc.players > cur.players && acc.players < 9)) {
+        return acc
+      } else return cur
+    }) */
+    getServers ? sv.redirectTo = getServers : sv.redirectTo = sv.serversInfos[0].ip
+    return sv
   })
+
+  serversJSON.lastSync = new Date().getTime()
+  serversJSON.servers = servers
+
+  response.json(serversJSON)
+  return writeFileSync('./servers.json', JSON.stringify(serversJSON))
 })
 
 app.listen(3333, () => console.log('Servidor rodando na porta 3333'))
